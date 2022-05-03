3440bcd49ec7411d536541ee3dcc304e1569be49
Anna Shveynfurt <57673883+AnnaShw@users.noreply.github.com>
2022-05-03 04:52:00 +0300

    Initial Commit & SignInUp frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+//import Custom components
+import SignInUpPage from "./SignInUpPage/SignInUpPage";
+import NavBar from "./NavBar/NavBar";
+//import css
+import "./App.css";
 
 function App() {
+  const [logedIn, setLogedIn] = useState<boolean>(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {logedIn && (
+        <React.Fragment>
+          <header className="App-header">
+            <NavBar />
+          </header>
+        </React.Fragment>
+      )}
+      {!logedIn && (
+        <main className="Stranger">
+          <SignInUpPage />
+        </main>
+      )}
+      <footer></footer>
     </div>
   );
 }
---
+++ src/NavBar/NavBar.tsx
@@ -0,0 +1,7 @@
+
+//import css
+import classes from './NavBar.module.css';
+
+export default function NavBar(){
+    return(<div></div>)
+}
